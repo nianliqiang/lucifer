c3c1534ce01f3a2339a828bba1b5f912244257f4
jonynian <jonynian@51jiecai.com>
2018-09-13 14:01:24 +0800

    init

--- src/main/java/com/lucifer/LuciferApplication.java
+++ src/main/java/com/lucifer/LuciferApplication.java
@@ -1,9 +1,14 @@
 package com.lucifer;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@MapperScan("com.jc.*.dao")
+@EnableScheduling
 public class LuciferApplication {
 
 	public static void main(String[] args) {
